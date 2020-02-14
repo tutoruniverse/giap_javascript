f221be1241a52b988c1f896df92ea4c5e2ad0b34
Casey Khuc <khuctrang1812@gmail.com>
2020-02-14 15:36:43 +0700

    chore: update demo app

--- demo/src/App.js
+++ demo/src/App.js
@@ -43,6 +43,12 @@ export default class App extends Component {
           }
         };
         break;
+      case 'setProfileProperties':
+        fields = ['propertyName', 'value'];
+        onSubmitClick = ({ propertyName, value }) => {
+          GIAP.setProfileProperties({ [propertyName]: value });
+        };
+        break;
       default:
         fields = [];
     }
@@ -55,43 +61,57 @@ export default class App extends Component {
   }
 
   render() {
+    const { modal } = this.state;
     return (
-      <React.Fragment>
-        <button
-          type="submit"
-          onClick={() => {
-            this.setState({ modal: 'track' });
-          }}
-        >
+      <div id="giap-app-container">
+        <div id="button-container">
+          <button
+            className={modal === 'track' ? 'button-active' : ''}
+            type="submit"
+            onClick={() => {
+              this.setState({ modal: 'track' });
+            }}
+          >
           Track
-        </button>
-        <button
-          type="submit"
-          onClick={() => {
-            this.setState({ modal: 'identify' });
-          }}
-        >
+          </button>
+          <button
+            className={modal === 'identify' ? 'button-active' : ''}
+            type="submit"
+            onClick={() => {
+              this.setState({ modal: 'identify' });
+            }}
+          >
           Sign In
-        </button>
-        <button
-          type="submit"
-          onClick={() => {
-            this.setState({ modal: 'alias' });
-          }}
-        >
+          </button>
+          <button
+            className={modal === 'alias' ? 'button-active' : ''}
+            type="submit"
+            onClick={() => {
+              this.setState({ modal: 'alias' });
+            }}
+          >
           Sign Up
-        </button>
-        <button
-          type="submit"
-          onClick={() => {
-            GIAP.reset();
-            this.setState({ modal: '' });
-          }}
-        >
+          </button>
+          <button
+            type="submit"
+            onClick={() => {
+              GIAP.reset();
+              this.setState({ modal: '' });
+            }}
+          >
           Sign Out
-        </button>
+          </button>
+          <button
+            type="submit"
+            onClick={() => {
+              this.setState({ modal: 'setProfileProperties' });
+            }}
+          >
+          Modify Profile
+          </button>
+        </div>
         {this.showModal()}
-      </React.Fragment>
+      </div>
     );
   }
 }
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/app.css';
-/* import './assets/css/custom.css'; */
+import './assets/css/custom.css';
 import App from './App';
 
 ReactDOM.render(
