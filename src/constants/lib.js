643fdca0729780ad530436bd69d220db6a120244
Tran Viet Thang <uendno@gmail.com>
2020-03-11 11:58:27 +0700

    chore: Update version

--- src/constants/lib.js
+++ src/constants/lib.js
@@ -4,7 +4,7 @@ export const QUEUE_LIMIT = 100;
 
 export const LIB = 'GIAP-javascript';
 
-export const LIB_VERSION = '1.0.1';
+export const LIB_VERSION = '1.1.1';
 
 export const PERSISTENCE_NAME = 'gotit.analytics.platform';
 
