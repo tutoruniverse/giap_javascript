1375adaeb851f825c0fc447a7434f91eafa18024
Tran Viet Thang <uendno@gmail.com>
2020-03-19 15:49:00 +0700

    fix: Take raw out put from jq

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
 then
-  VERSION=`jq .version package.json`
+  VERSION=`jq -r .version package.json`
   REACT_APP_ENV=`echo ${REACT_APP_ENV} | tr [a-z] [A-Z]`
   echo "s3://$S3"
   aws s3 sync --delete --cache-control max-age=2592000,public umd/ s3://$S3/$VERSION
